0e87ff0a2f657ae83dca2c7cbdb564b170b0bd1d
HexTeke <yianni.whisler@gmail.com>
2022-04-17 15:36:50 -0500

    created home and about pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './Home';
+import About from './About';
+import {Route, Link} from 'react-router-dom';
+import UnderConstruction from './UnderConstruction';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Under Construction.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Route exact path="/" component={UnderConstruction} />
+      <Route exact path="/home" component={Home} />
+      <Route exact path="/about" component={About} />
     </div>
   );
 }
