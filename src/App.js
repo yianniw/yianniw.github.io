1951fded2e53e400821dba63f0d4d2d8c1585431
HexTeke <yianni.whisler@gmail.com>
2022-04-18 02:24:10 -0500

    added LoLgress home

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from './Home';
 import About from './About';
 import {Route, Link} from 'react-router-dom';
 import UnderConstruction from './UnderConstruction';
+import LoLgress from './LoLgress';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Route exact path="/" component={UnderConstruction} />
       <Route exact path="/home" component={Home} />
       <Route exact path="/about" component={About} />
+      <Route exact path="/LoLgress" component={LoLgress} />
     </div>
   );
 }
